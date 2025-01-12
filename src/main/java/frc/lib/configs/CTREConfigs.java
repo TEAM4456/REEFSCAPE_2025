facbe822ae7ae60592d9ca7452bc1116d8a7138e
Connor Bliss <cbliss@stjohnschs.org>
2025-01-11 22:14:32 -0500

    trying to update ctre configs

--- src/main/java/frc/lib/configs/CTREConfigs.java
+++ src/main/java/frc/lib/configs/CTREConfigs.java
@@ -3,17 +3,19 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.lib.configs;
-import com.ctre.phoenix.sensors.AbsoluteSensorRange;
-import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.AbsoluteSensorRange; /*don't think this is used?  if it is here is documentation on 2025 migration
+https://v6.docs.ctr-electronics.com/en/stable/docs/yearly-changes/yearly-changelog.html (look at deprecations/removals) */
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 import com.ctre.phoenix.sensors.SensorTimeBase;
 import frc.robot.Constants;
 
 public final class CTREConfigs {
-  public CANCoderConfiguration swerveCanCoderConfig;
+  public CANcoderConfiguration swerveCanCoderConfig;
 
   public CTREConfigs() {
-    swerveCanCoderConfig = new CANCoderConfiguration();
+    swerveCanCoderConfig = new CANcoderConfiguration();
+    /*look to see if there are any samples online of the "Swerve CANCoder Configuration" listed below online (migrated for 2025) */
 
     /* Swerve CANCoder Configuration */
     swerveCanCoderConfig.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
