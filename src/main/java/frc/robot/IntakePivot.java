c6ecbaea98681c0be0b4c72d70a886e1556c6e52
Connor Bliss <cbliss@stjohnschs.org>
2025-01-30 16:08:48 -0500

    Add intakePivot

---
+++ src/main/java/frc/robot/IntakePivot.java
@@ -0,0 +1,102 @@
+/*
+Below is copied exactly from elevator.java
+*/
+package frc.robot.Subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class IntakePivot extends SubsystemBase {
+    private SparkMax intakePivotMotor;
+    private RelativeEncoder intakePivotEncoder; 
+    private SparkClosedLoopController intakePivotPIDController;
+    private SparkMaxConfig intakePivotConfig;
+
+    public intakePivot() {
+        intakePivotMotor = new SparkMax(19, MotorType.kBrushless);
+        intakePivotEncoder = intakePivotMotor.getEncoder();
+        intakePivotConfig = new SparkMaxConfig();
+        intakePivotConfig.idleMode(IdleMode.kBrake);
+        intakePivotConfig.closedLoop.pid(1,0,0);
+}
+
+public class intakePivot extends SubsystemBase {
+    private SparkMax intakePivotMotor;
+    private RelativeEncoder intakePivotEncoder; 
+    private SparkClosedLoopController intakePivotPIDController;
+    private SparkMaxConfig intakePivotConfig;
+
+    /*  // look into getting rid of this? Not sure if needed? Sat 1/25
+   //elevatorMotor.setOpenLoopRampRate(.5);
+    elevatorEncoder = elevatorMotor.getEncoder();
+    elevatorPIDController = elevatorMotor.getClosedLoopController();
+
+     //elevatorPIDController.pid(1, 0, 0);
+     elevatorPIDController.setFF(0); */
+   }
+  
+  /* Create your elevator Methods (? correct terminology) here */
+
+  //When called, this moves the motor at negative elevatorSpeed to go up
+  public void intakePivotUp(){
+    intakePivotMotor.set(-Constants.ElevatorPositions.elevatorSpeed);
+  }
+  //When called, this moves the moter at positive elevatorSpeed to go down
+  public void intakePivotDown(){
+    intakePivotMotor.set(Constants.ElevatorPositions.elevatorSpeed);
+  }
+   public void intakePivotStop(){
+    intakePivotMotor.set(0);
+}
+//     elevatorLeftPIDController.setReference(elevatorLeftEncoder.getPosition(), SparkMax.ControlType.kPosition);
+//     elevatorRightPIDController.setReference(elevatorRightEncoder.getPosition(), SparkMax.ControlType.kPosition);
+  
+
+//   public void setElevatorPositionUp(){
+//     elevatorLeftPIDController.setReference(Constants.ElevatorPositions.leftElevatorUp, SparkMax.ControlType.kPosition);
+//     elevatorRightPIDController.setReference(Constants.ElevatorPositions.rightElevatorUp, SparkMax.ControlType.kPosition);
+//   }
+//   public void setElevatorPositionDown(){
+//     elevatorLeftPIDController.setReference(Constants.ElevatorPositions.leftElevatorDown, SparkMax.ControlType.kPosition);
+//     elevatorRightPIDController.setReference(Constants.ElevatorPositions.rightElevatorDown, SparkMax.ControlType.kPosition);
+//   }
+//   
+
+  /*Create manually controlled commands here */
+
+   public Command intakePivotUpCommand(){
+      return run(() -> intakePivotUp()).withTimeout(0.1);
+    }
+
+    public Command intakePivotDownCommand(){
+      return run(() -> intakePivotDown()).withTimeout(0.1);
+    }
+
+    public Command intakePivotStopCommand(){
+      return run(() -> intakePivotStop());
+    }
+
+    /*Create set position commands here */
+
+//  public Command setElevatorPositionUpCommand(){
+//     return run(() -> setElevatorPositionUp()).until(() -> (Math.abs(elevatorRightEncoder.getPosition() - Constants.ElevatorPositions.rightElevatorUp) < 1) && (Math.abs(elevatorLeftEncoder.getPosition() - Constants.ElevatorPositions.leftElevatorUp) < 1));
+//   }
+//   public Command setElevatorPositionDownCommand(){
+//     return run(() -> setElevatorPositionDown()).until(() -> (Math.abs(elevatorRightEncoder.getPosition() - Constants.ElevatorPositions.rightElevatorDown) < 1) && (Math.abs(elevatorLeftEncoder.getPosition() - Constants.ElevatorPositions.leftElevatorDown) < 1));
+//   }
+  @Override
+    public void periodic(){
+      SmartDashboard.putNumber("intakePivotPosition",intakePivotEncoder.getPosition());
+    }
+  }
+
