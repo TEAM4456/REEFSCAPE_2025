384dcd3b8159c0a5e6465c377ccb9db0021cad08
Connor Bliss <cbliss@stjohnschs.org>
2025-02-08 10:50:31 -0500

    made left and right versions of intake

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -15,19 +15,30 @@ import frc.robot.Constants;
 
 
 public class Intake extends SubsystemBase {
-    private SparkMax intakeMotor;
-    private RelativeEncoder intakeEncoder; 
-    private SparkClosedLoopController intakePIDController;
-    private SparkMaxConfig intakeConfig;
+    private SparkMax rightIntakeMotor;
+    private RelativeEncoder rightIntakeEncoder; 
+    private SparkClosedLoopController rightIntakePIDController;
+    private SparkMaxConfig rightIntakeConfig;
+
+    private SparkMax leftIntakeMotor;
+    private RelativeEncoder leftIntakeEncoder; 
+    private SparkClosedLoopController leftIntakePIDController;
+    private SparkMaxConfig leftIntakeConfig;
 
   public Intake() {
 
     //Make a right and left intake for this subsystem, CanID 19 and 20
-    intakeMotor = new SparkMax(19, MotorType.kBrushless);
-    intakeEncoder = intakeMotor.getEncoder();
-    intakeConfig = new SparkMaxConfig();
-    intakeConfig.idleMode(IdleMode.kBrake);
-    intakeConfig.closedLoop.pidf(1,0,0,0);
+    rightIntakeMotor = new SparkMax(19, MotorType.kBrushless);
+    rightIntakeEncoder = rightIntakeMotor.getEncoder();
+    rightIntakeConfig = new SparkMaxConfig();
+    rightIntakeConfig.idleMode(IdleMode.kBrake);
+    rightIntakeConfig.closedLoop.pidf(1,0,0,0);
+
+    leftIntakeMotor = new SparkMax(19, MotorType.kBrushless);
+    leftIntakeEncoder = rightIntakeMotor.getEncoder();
+    leftIntakeConfig = new SparkMaxConfig();
+    leftIntakeConfig.idleMode(IdleMode.kBrake);
+    leftIntakeConfig.closedLoop.pidf(1,0,0,0);
 
     /*  // look into getting rid of this? Not sure if needed? Sat 1/25
    //intakeMotor.setOpenLoopRampRate(.5);
@@ -40,36 +51,62 @@ public class Intake extends SubsystemBase {
   
   /* Create your intake Methods here */
 
-  //When called, this moves the motor at negative intakeSpeed to go up
-  public void intakePickupCoral(){
-    intakeMotor.set(-Constants.IntakeSpeeds.intakePickupCoral);
+  //When called, this moves right motor at negative intakeSpeed to go up
+  public void rightIntakePickupCoral(){
+    rightIntakeMotor.set(-Constants.IntakeSpeeds.intakePickupCoral);
   }
-  //When called, this moves the moter at positive elevatorSpeed to go down
-  public void intakeScoreCoral(){
-    intakeMotor.set(Constants.IntakeSpeeds.intakeScoreCoral);
+  //When called, this moves right motor at positive elevatorSpeed to go down
+  public void rightIntakeScoreCoral(){
+    rightIntakeMotor.set(Constants.IntakeSpeeds.intakeScoreCoral);
   }
-   public void intakeStop(){
-     intakeMotor.set(0);
+   public void rightIntakeStop(){
+     rightIntakeMotor.set(0);
+}
+ //When called, this moves left motor at intakeSpeed to go up
+ public void leftIntakePickupCoral(){
+  leftIntakeMotor.set(Constants.IntakeSpeeds.intakePickupCoral);
+}
+//When called, this moves left motor at positive elevatorSpeed to go down
+public void leftIntakeScoreCoral(){
+  leftIntakeMotor.set(Constants.IntakeSpeeds.intakeScoreCoral);
+}
+ public void leftIntakeStop(){
+   leftIntakeMotor.set(0);
 }
 
   /*Create manually controlled commands here */
 
-   public Command intakePickupCoralCommand(){
-      return run(() -> intakePickupCoral());
+   public Command rightIntakePickupCoralCommand(){
+      return run(() -> rightIntakePickupCoral());
     }
 
-    public Command intakeScoreCoralCommand(){
-      return run(() -> intakeScoreCoral());
+    public Command rightIntakeScoreCoralCommand(){
+      return run(() -> rightIntakeScoreCoral());
     }
 
-    public Command intakeStopCommand(){
-      return run(() -> intakeStop());
+    public Command rightIntakeStopCommand(){
+      return run(() -> rightIntakeStop());
+    }
+
+
+
+    public Command leftIntakePickupCoralCommand(){
+      return run(() -> leftIntakePickupCoral());
+    }
+
+    public Command leftIntakeScoreCoralCommand(){
+      return run(() -> leftIntakeScoreCoral());
+    }
+
+    public Command leftIntakeStopCommand(){
+      return run(() -> leftIntakeStop());
     }
 
     /*Create set position commands here */
 
   @Override
     public void periodic(){
-      SmartDashboard.putNumber("intakePosition",intakeEncoder.getPosition());
+      SmartDashboard.putNumber("intakePosition",rightIntakeEncoder.getPosition());
+      SmartDashboard.putNumber("intakePosition",leftIntakeEncoder.getPosition());
     }
   }
